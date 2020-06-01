626b0f633496e8b7889824346e762822d856c987
Reinis Cirpons <rc234@st-andrews.ac.uk>
2020-06-01 12:24:39 +0100

    Add python prototype stub

---
+++ python/prototype.py
@@ -0,0 +1,8 @@
+# Given a string x representing a free band element, return its content
+#  defined as the set of letters that occur in it
+def content_of_string(x):
+    # To do: Finish complement of string function
+    pass
+
+a = "aabcdcdabcacd"
+print(content_of_string(a))
